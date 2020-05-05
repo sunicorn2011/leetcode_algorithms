06c5ee4f7696e35e10005bdb82f400dad54a8aa4
chenjun0123 <junchen.unif@qq.com>
2020-05-05 10:18:35 +0800

    update

---
+++ java/N700.java
@@ -0,0 +1,8 @@
+public class N700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        if (root.val < val) return searchBST(root.right, val);
+        return searchBST(root.left, val);
+    }
+}
